b0a0d77a37ea6c20735a2523bebce793e1535987
Uladzislau Marshalau <marshal25@yandex.by>
2024-12-27 21:39:58 +0300

    attachment: created mapper for  attachment

---
+++ src/main/java/com/school/mapper/AttachmentMapper.java
@@ -0,0 +1,42 @@
+package com.school.mapper;
+
+import com.school.dto.AttachmentDtoForSend;
+import com.school.entity.Attachment;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttachmentMapper {
+    public AttachmentDtoForSend attachmentToAttachmentDtoForSend(Attachment entity, byte[] data) {
+        AttachmentDtoForSend dto = new AttachmentDtoForSend(
+                entity.getAttachmentTitle(),
+                entity.getUploadDate(),
+                entity.getExtension(),
+                determineMediaType(entity.getExtension()),
+                data
+        );
+        return dto;
+    }
+
+    private MediaType determineMediaType(String extension) {
+        switch (extension) {
+            case ".jpg":
+            case ".jpeg":
+                return MediaType.IMAGE_JPEG;
+            case ".png":
+                return MediaType.IMAGE_PNG;
+            case ".gif":
+                return MediaType.IMAGE_GIF;
+            case ".pdf":
+                return MediaType.APPLICATION_PDF;
+            case ".txt":
+                return MediaType.TEXT_PLAIN;
+            case ".html":
+                return MediaType.TEXT_HTML;
+            case ".json":
+                return MediaType.APPLICATION_JSON;
+            default:
+                return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+}
